2f0a5eafd2f4a900db67272b496ae2585c678202
Will Mildenhall <wtmildenhall@gmail.com>
2018-09-28 13:23:20 -0600

    about us page

---
+++ web/about-us.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fake website about us page.</title>
+</head>
+<body>
+	<h1>All About Us</h1>
+	<p>We are all about creating the perfect igloo. We provide the blueprints,<br>
+	you provide the manpower. Got any ideas for a new igloo style? Let us know<br>
+	by contacting us at 1-800-IGLOO</p><br>
+</body>
+</html>
