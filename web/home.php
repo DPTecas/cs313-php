d3c2165532664b9fd2db923c9db5554462733698
Will Mildenhall <wtmildenhall@gmail.com>
2018-09-28 13:27:14 -0600

    login and home pages

---
+++ web/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Igloos R Us</h1>
+	<p>Welcome. You are not logged in.</p>
+</body>
+</html>
---
+++ web/login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login page</title>
+</head>
+<body>
+	<h1>Igloos R Us</h1>
+	<button>Log in as Administrator</button>
+	<button>Log in as Tester</button>
+</body>
+</html>
