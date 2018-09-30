7ac1b97b96b547db37a5ad68c43a8343de7672d3
Will Mildenhall <wtmildenhall@gmail.com>
2018-09-29 19:04:28 -0600

    too many commits

--- web/homepage.php
+++ web/homepage.php
@@ -11,11 +11,24 @@
 	<div id = "hobbies">Hobbies Include - 
 		<ul>
 			<?php 
-				$hobbies = array("Model Building", "Guitar", "Gaming", "Ping Pong");
+				$hobbies = array("Model Building", "Guitar", "Gaming", "Ping Pong, Basketball");
 				foreach ($hobbies as $val){
 						echo "<li>" . $val . "<br>";
 						}?>
 		</ul>
 	</div>
+
+	<div id = "models">Some Models I've Built
+		<img id = "tank" src="MEPics/tank.png" alt="ME">
+		<img src="MEPics/r2d2.png" alt="ME">
+		<img src="MEPics/pearl.png" alt="ME">
+	</div>
+
+	<div id="myModal" class="modal">
+  		<span class="close">&times;</span>
+  		<img class="modal-content" id="img01">
+    <div id="caption"></div>
+
+    <script src="homejs.js"></script>
 </body>
 </html>
