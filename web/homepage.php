24691010720c4708db5e6e2e35420c76995d655b
Will Mildenhall <wtmildenhall@gmail.com>
2018-09-29 18:31:10 -0600

    main + hobbies

--- web/homepage.php
+++ web/homepage.php
@@ -5,6 +5,17 @@
 	<title>Home Page CS313</title>
 </head>
 <body>
+	<p id = "name">Will Mildenhall</p><br>
+	<p id = "class">Web Engineering II</p><br>
 
+	<div>Hobbies Include - 
+		<ul>
+			<?php 
+				$hobbies = array("Model Building", "Guitar", "Gaming", "Ping Pong");
+				foreach ($hobbies as $val){
+						echo "<li>" . $val . "<br>";
+						}?>
+		</ul>
+	</div>
 </body>
 </html>
