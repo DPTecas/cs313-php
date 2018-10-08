5654e5da891d2daa4faadca588d1403b4e9ee918
Will Mildenhall <wtmildenhall@gmail.com>
2018-10-08 17:47:11 -0600

    changes

--- web/Prove03/shoppingCart.php
+++ web/Prove03/shoppingCart.php
@@ -10,16 +10,16 @@
 	$_SESSION['totalPrice'] = ((isset($_SESSION['totalPrice'])) ? $_SESSION['totalPrice'] : 0);
 
 	if (isset($_SESSION['cart1']))
-		echo "Carbon Fiber Paddles: " . $_SESSION['cart1'] . " = " . $_SESSION['cart1'] * 30.00;
+		echo "Carbon Fiber Paddles: " . $_SESSION['cart1'] . " = \$" . $_SESSION['cart1'] * 30.00 . "<br>";
 
 	if (isset($_SESSION['cart2']))
-		echo "Carbon Fiber Paddles: " . $_SESSION['cart2'] . " = " . $_SESSION['cart2'] * 30.00;
+		echo "Couples Package: " . $_SESSION['cart2'] . " = \$" . $_SESSION['cart2'] * 40.00 . "<br>";
 
 	if (isset($_SESSION['cart3']))
-		echo "Carbon Fiber Paddles: " . $_SESSION['cart3'] . " = " . $_SESSION['cart3'] * 30.00;
+		echo "One Orange Ball: " . $_SESSION['cart3'] . " = \$" . $_SESSION['cart3'] * 0.50 . "<br>";
 
 	if (isset($_SESSION['cart4']))
-		echo "Carbon Fiber Paddles: " . $_SESSION['cart4'] . " = " . $_SESSION['cart4'] * 30.00;
+		echo "One White Ball: " . $_SESSION['cart4'] . " = \$" . (float)$_SESSION['cart4'] * 0.50 . "<br>";
 	
 	echo "Total Price: \$" . $_SESSION['totalPrice'];
 	
