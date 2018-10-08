c3997431458b50e3b46c9cc1b7908f3ecf7a1c34
Will Mildenhall <wtmildenhall@gmail.com>
2018-10-08 11:05:59 -0600

    session stuff

--- web/Prove03/prove03.php
+++ web/Prove03/prove03.php
@@ -5,10 +5,27 @@
 	<link rel="stylesheet" type="text/css" href="prove03.css">
 </head>
 <body>
+	<?php
+	session_start();
+	?>
 	
     <button type="button" class="sCart">
         <span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart
     </button>
+
+    <div class = item>
+    	<img src = "pics/carbonFiberPaddle.jpg" alt = "Carbon Fiber Paddle" class = "itemImages"><br>
+    	<p>Carbon Fiber Paddle $30.00</p><br>
+    	<input type = "submit" name = "item1" value = "Add to Cart">
+    	<?php
+    	$_SESSION["item1"] = 0
+    	if ($SERVER['REQUEST_METHOD'] === 'POST'){
+    		if (isset($_POST['item1'])){
+    			$_SESSION["item1"] += 1
+    		}
+    	}
+    	?>
+    </div>
     
 </body>
 </html>
