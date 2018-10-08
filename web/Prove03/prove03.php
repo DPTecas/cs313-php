de9ce0dec1ad8d00d1af9b7073b67320f1953156
Will Mildenhall <wtmildenhall@gmail.com>
2018-10-08 12:40:34 -0600

    more items

--- web/Prove03/prove03.php
+++ web/Prove03/prove03.php
@@ -18,15 +18,50 @@
     	<input type = "submit" name = "item1" value = "Add to Cart">
     	</form>
     </div>
+
+    <div class = item>
+    	<img src = "pics/twoPaddles.jpg" alt = "Two Paddle" class = "itemImages"><br>
+    	<p>Couples Package $40.00</p><br>
+    	<form action ="<? echo $_SERVER['PHP_SELF']; ?>" method = "get">
+    	<input type = "submit" name = "item2" value = "Add to Cart">
+    	</form>
+    </div>
+
+    <div class = item>
+    	<img src = "pics/singleOrange.jpg" alt = "Orange Ball" class = "itemImages"><br>
+    	<p>Single Orange Ping Pong Ball $0.50</p><br>
+    	<form action ="<? echo $_SERVER['PHP_SELF']; ?>" method = "get">
+    	<input type = "submit" name = "item3" value = "Add to Cart">
+    	</form>
+    </div>
+
+    <div class = item>
+    	<img src = "pics/singleWhite.jpg" alt = "White Ball" class = "itemImages"><br>
+    	<p>Single White Ping Pong Ball $0.50</p><br>
+    	<form action ="<? echo $_SERVER['PHP_SELF']; ?>" method = "get">
+    	<input type = "submit" name = "item4" value = "Add to Cart">
+    	</form>
+    </div>
     <?php
     	$_SESSION['cart1'] = ((isset($_SESSION['cart1'])) ? $_SESSION['cart1'] : 0);
-    	
-    	
     	if (isset($_GET["item1"])){
     		echo ++$_SESSION['cart1'];
     	}
-    	
-    	
+
+    	$_SESSION['cart2'] = ((isset($_SESSION['cart2'])) ? $_SESSION['cart2'] : 0);
+    	if (isset($_GET["item2"])){
+    		echo ++$_SESSION['cart2'];
+    	}	
+
+    	$_SESSION['cart3'] = ((isset($_SESSION['cart3'])) ? $_SESSION['cart3'] : 0);
+    	if (isset($_GET["item3"])){
+    		echo ++$_SESSION['cart3'];
+    	}	
+
+    	$_SESSION['cart4'] = ((isset($_SESSION['cart4'])) ? $_SESSION['cart4'] : 0);
+    	if (isset($_GET["item4"])){
+    		echo ++$_SESSION['cart4'];
+    	}		
     ?>
     
 </body>
