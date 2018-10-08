d638a3271121ff4c24ce72fd5da486d3996880fe
Will Mildenhall <wtmildenhall@gmail.com>
2018-10-07 21:20:03 -0600

    saved

--- web/Prove03/prove03.php
+++ web/Prove03/prove03.php
@@ -2,8 +2,13 @@
 <html>
 <head>
 	<title>Prove 03 Shopping Cart</title>
+	<link rel="stylesheet" type="text/css" href="prove03.css">
 </head>
 <body>
-
+	
+    <button type="button" class="sCart">
+        <span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart
+    </button>
+    
 </body>
 </html>
