8f7c9fff99ac0b1b588a64e8eafbdee7951c008f
Will Mildenhall <wtmildenhall@gmail.com>
2018-10-08 18:28:29 -0600

    changes

--- web/Prove03/checkout.php
+++ web/Prove03/checkout.php
@@ -4,6 +4,7 @@
 	<title>Checkout Page</title>
 </head>
 <body>
+
 	<form action="confirmation.php" method="post">
 	Address: <input type="text" name="address"><br>
 	<input type="submit">
--- web/Prove03/confirmation.php
+++ web/Prove03/confirmation.php
@@ -4,9 +4,36 @@
 	<title></title>
 </head>
 <body>
-	<p>Purchase confirmed<br></p>
 	<?php
-	echo "Address: " . $_POST['address'];
+	session_start();
+
+	$_SESSION['totalPrice'] = ((isset($_SESSION['totalPrice'])) ? (float)$_SESSION['totalPrice'] : 0);
+
+	if (isset($_SESSION['cart1']))
+		echo "Carbon Fiber Paddles: " . $_SESSION['cart1'] . " = \$" . (float)$_SESSION['cart1'] * 30.00 . "<br>";
+
+	if (isset($_SESSION['cart2']))
+		echo "Couples Package: " . $_SESSION['cart2'] . " = \$" . (float)$_SESSION['cart2'] * 40.00 . "<br>";
+
+	if (isset($_SESSION['cart3']))
+		echo "One Orange Ball: " . $_SESSION['cart3'] . " = \$" . (float)$_SESSION['cart3'] * 0.50 . "<br>";
+
+	if (isset($_SESSION['cart4']))
+		echo "One White Ball: " . $_SESSION['cart4'] . " = \$" . (float)$_SESSION['cart4'] * 0.50 . "<br>";
+	
+	echo "Total Price: \$" . (float)$_SESSION['totalPrice'] . "<br>";
+	?>
+
+	<p>Purchase confirmed<br></p>
+
+	<?php
+	function test_input($data) {
+  		$data = trim($data);
+  		$data = stripslashes($data);
+  		$data = htmlspecialchars($data);
+  		return $data;
+}
+	echo "Address: " . test_input($_POST['address']);
 	?>
 
 </body>
