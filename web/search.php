ee4af08ed4c0e41e7e3be59f648c0c6b675c5e96
Will Mildenhall <wtmildenhall@gmail.com>
2018-10-16 16:02:31 -0600

    teach05

--- web/search.php
+++ web/search.php
@@ -1,21 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Teach 05</title>
+	<title>searching</title>
 </head>
 <body>
-
-	<form action="search.php" method="get">
-	Book: <input type="text" name="book"><br>
-	<input type="submit">
-	</form>
-
 	<?php
 
 	ini_set("display_errors", 1); error_reporting(E_ALL);
 	require ("dbConnect.php");
 
-	foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+	$var = $_GET['book'];
+
+	foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures WHERE book = $var') as $row)
 	{
 		echo $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "<br>" . $row['content'] . "<br>";
 
