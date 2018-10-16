2e6cc0c6f50605eade0b898762c3bdc6bc8b7edb
Will Mildenhall <wtmildenhall@gmail.com>
2018-10-16 16:06:01 -0600

    teach05

--- web/search.php
+++ web/search.php
@@ -9,9 +9,9 @@
 	ini_set("display_errors", 1); error_reporting(E_ALL);
 	require ("dbConnect.php");
 
-	$var = $_GET['book'];
+	$var = "'" . $_GET['book'] . "'";
 
-	foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures WHERE book = '$var'') as $row)
+	foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures WHERE book = $var') as $row)
 	{
 		echo $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "<br>" . $row['content'] . "<br>";
 
