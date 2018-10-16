77b3a50c5d7c0041356212af95c4ae7205aab841
Will Mildenhall <wtmildenhall@gmail.com>
2018-10-16 16:31:39 -0600

    teach05

--- web/search.php
+++ web/search.php
@@ -13,8 +13,11 @@
 
 	foreach ($db->query("SELECT book, chapter, verse FROM scriptures WHERE book = $var") as $row)
 	{
-
-		$search = "?book=$row['book']&chapter=$row['chapter']&verse=$row['verse']";
+		$book = $row['book'];
+		$chapter = $row['chapter'];
+		$verse = $row['verse'];
+		
+		$search = "?book=$book&chapter=$chapter&verse=$verse";
 		$scrip = $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "<br>";
 		//echo "<a href=\"https://quiet-temple-53214.herokuapp.com/scripInfo.php$search\">$scrip</a>";
 		echo "$search";
