2a0c9f0ddedc70b874742ada636d580533a313b6
Will Mildenhall <wtmildenhall@gmail.com>
2018-10-16 15:55:48 -0600

    teach05

--- web/teach05.php
+++ web/teach05.php
@@ -5,6 +5,8 @@
 </head>
 <body>
 	<?php
+
+	ini_set("display_errors", 1); error_reporting(E_ALL);
 	require ("dbConnect.php");
 
 	foreach ($db->query('SELECT username, password FROM note_user') as $row)
