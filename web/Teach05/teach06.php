3351e7f86656b41299660310d68c3f703a872212
Will Mildenhall <wtmildenhall@gmail.com>
2018-10-23 16:19:36 -0600

    teach06

--- web/Teach05/teach06.php
+++ web/Teach05/teach06.php
@@ -11,20 +11,23 @@
 	Verse: <input type="text" name="verse"><br>
 	Content: <textarea rows="4" cols="50" name="content"></textarea><br>
 	<input type="submit">
-	</form>
+	
 
 	<?php
-
 	ini_set("display_errors", 1); error_reporting(E_ALL);
 	require ("dbConnect.php");
 
-	foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+	foreach ($db->query('SELECT * FROM topic') as $row)
 	{
-		echo $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "<br>" . $row['content'] . "<br>";
+		$id = $row['id'];
+		$topic = $row['topic'];
+		echo "<input type="checkbox" name = "topic" value = "$id"> $topic<br>";
 
 	}
 
 	?>
+
+	</form>
 
 </body>
 </html>
