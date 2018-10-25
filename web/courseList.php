2d80286b4b6820049c3eea06bd3cf2f6289b7e19
Will Mildenhall <wtmildenhall@gmail.com>
2018-10-25 16:41:12 -0600

    in class

--- web/courseList.php
+++ web/courseList.php
@@ -3,11 +3,11 @@
 
 	$db = get_db();
 
-	#$query = 'SELECT id, code, name FROM course';
+	$query = 'SELECT id, code, name FROM course';
 
-	#$stmt = $db->prepare($query);
-	#$stmt->execute();
-	#$courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	$stmt = $db->prepare($query);
+	$stmt->execute();
+	$courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -21,13 +21,13 @@
 
 	<ul>
 	<?php
-#		foreach ($courses as $course) {
-#			$id = $course['id'];
-#			$name = $course['name'];
-#			$code = $course['code'];
+		foreach ($courses as $course) {
+			$id = $course['id'];
+			$name = $course['name'];
+			$code = $course['code'];
 
-			//echo "<li><p>$code - $name</p></li>\n";
-#		}
+			echo "<li><p>$code - $name</p></li>\n";
+		}
 	?>
 	</ul>
 
--- web/dbConnect.php
+++ web/dbConnect.php
@@ -1,25 +1,39 @@
 <?php
-	try
-	{
-	  $dbUrl = getenv('DATABASE_URL');
+/**********************************************************
+* File: dbConnect.php
+* Author: Br. Burton
+* 
+* Description: Shows how to connect with Heroku credentials.
+***********************************************************/
 
-	  $dbOpts = parse_url($dbUrl);
+function get_db() {
+    $db = NULL;
 
-	  $dbHost = $dbOpts["host"];
-	  $dbPort = $dbOpts["port"];
-	  $dbUser = $dbOpts["user"];
-	  $dbPassword = $dbOpts["pass"];
-	  $dbName = ltrim($dbOpts["path"],'/');
+    try {
+        // default Heroku Postgres configuration URL
+        $dbUrl = getenv('DATABASE_URL');
 
-	  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        // Get the various parts of the DB Connection from the URL
+        $dbopts = parse_url($dbUrl);
 
-	  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-	catch (PDOException $ex)
-	{
-	  echo 'Error!: ' . $ex->getMessage();
-	  die();
-	}
+        $dbHost = $dbopts["host"];
+        $dbPort = $dbopts["port"];
+        $dbUser = $dbopts["user"];
+        $dbPassword = $dbopts["pass"];
+        $dbName = ltrim($dbopts["path"],'/');
 
+        // Create the PDO connection
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-?>
+        // this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+        $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch (PDOException $ex) {
+        // If this were in production, you would not want to echo
+        // the details of the exception.
+        echo "Error connecting to DB. Details: $ex";
+        die();
+    }
+
+    return $db;
+}
