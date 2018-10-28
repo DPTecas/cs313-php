8fe8424777037b8ec5543150eccaf416b5abed80
Will Mildenhall <wtmildenhall@gmail.com>
2018-10-27 21:17:11 -0600

    project

--- web/Project/result.php
+++ web/Project/result.php
@@ -14,14 +14,24 @@
 
 	$entry = $_GET['entry'];
 
-	$query = 'UPDATE entries SET score = 0 WHERE entry = :entry';
+	$query2 = 'SELECT score FROM entries WHERE entry = :entry';
+	$stmt = $db->prepare($query2);
+	$stmt->bindValue(':entry', $entry, PDO::PARAM_STR);
+	$stmt->execute();
+	$scores = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	$scores[0]['score'] += 1 
 
+	$query = 'UPDATE entries SET score = :score WHERE entry = :entry';
 	$stmt = $db->prepare($query);
-	$stmt->bindValue(':entry', $id, PDO::PARAM_STR);
+	$stmt->bindValue(':entry', $entry, PDO::PARAM_STR);
+	$stmt->bindValue(':score', $scores[0]['score'], PDO::PARAM_INT);
 	$stmt->execute();
 	$entries = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+	
+
 	echo "The winner is $entry";
+	echo "Other people who think this is number one: " . $scores[0]['score'];
 	?>
 
 </body>
