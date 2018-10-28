de7573c8b7983a94fded4fbea0e056728db8ef94
Will Mildenhall <wtmildenhall@gmail.com>
2018-10-27 19:46:54 -0600

    project

--- web/Project/bracket.js
+++ web/Project/bracket.js
@@ -1,6 +1,6 @@
 document.getElementById('b0').onclick = function() {
    
-   document.getElementById("b0").style.display="none";
+   this.style.display="none";
    document.getElementById("b1").style.display="none";
 
    document.getElementById("c1").value=this.value;
