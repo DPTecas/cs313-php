b7f89a4da755a8ffc3fd6ac386195a29bb2b36c2
Will Mildenhall <wtmildenhall@gmail.com>
2018-11-03 19:49:14 -0600

    bracket maker stuff

--- web/Project/submitted.php
+++ web/Project/submitted.php
@@ -25,16 +25,17 @@
 	$stmt->execute();
 	$tourney = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-	$query3 = 'INSERT INTO entries (entry, tourney_id) VALUES (:entry, :id)';
-	$stmt = $db->prepare($query3);
+//	$query3 = 'INSERT INTO entries (entry, tourney_id) VALUES (:entry, :id)';
+//	$stmt = $db->prepare($query3);
+//	$stmt->bindValue(':entry', $entry, PDO::PARAM_STR);
+//		$stmt->bindValue(':id', $tourney['id'], PDO::PARAM_INT);
+//		$stmt->execute();
+//		$recentEntry = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 	for ($i=1;$i<17;$i++) {
 		$entry = "e" . $i;
 		$entry = $_POST[$entry];
-			
-		$stmt->bindValue(':entry', $entry, PDO::PARAM_STR);
-		$stmt->bindValue(':id', $tourney['id'], PDO::PARAM_INT);
-		$stmt->execute();
+		echo $entry;
 	}
 	?>
 
