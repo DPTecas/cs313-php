9b9925567500316be3cdbd3529bd628ea77855dc
Will Mildenhall <wtmildenhall@gmail.com>
2018-11-03 19:24:16 -0600

    bracket maker stuff

--- web/Project/submitted.php
+++ web/Project/submitted.php
@@ -18,23 +18,23 @@
 	$stmt->bindValue(':title', $title, PDO::PARAM_STR);
 	$stmt->execute();
 	$tname = $stmt->fetchAll(PDO::FETCH_ASSOC);
-/*
+
 	$query = 'SELECT * FROM tourneys WHERE tname = :title';
 	$stmt = $db->prepare($query);
 	$stmt->bindValue(':title', $tname['tname'], PDO::PARAM_STR);
 	$stmt->execute();
-	$tourney = $stmt->fetchAll(PDO::FETCH_ASSOC);*/
-/*
+	$tourney = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 	for ($i=1;$i<17;$i++) {
 		$entry = "e" . $i;
-		$entry = "'" . $_POST[$entry] . "'";
+		$entry = $_POST[$entry];
 
 		$query = 'INSERT INTO entries (entry, tourney_id) VALUES (:entry, :id)';
 		$stmt = $db->prepare($query);
 		$stmt->bindValue(':entry', $entry, PDO::PARAM_STR);
 		$stmt->bindValue(':id', $tourney['id'], PDO::PARAM_INT);
 		$stmt->execute();
-	}*/
+	}
 	?>
 
 </body>
